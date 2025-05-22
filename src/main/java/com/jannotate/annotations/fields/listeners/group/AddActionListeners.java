5e9e1d10b19c79f55dcfb66fa561d958cf1821ac
luchersol <lmherencia2003@gmail.com>
2025-05-23 00:48:17 +0200

    Remove annotation

--- src/main/java/com/jannotate/annotations/fields/listeners/group/AddActionListeners.java
+++ src/main/java/com/jannotate/annotations/fields/listeners/group/AddActionListeners.java
@@ -6,11 +6,9 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 import com.jannotate.annotations.fields.listeners.single.AddActionListener;
-import com.jannotate.common.annotations.ContainerAnnotation;
 
 @Target(ElementType.FIELD)
 @Retention(RetentionPolicy.RUNTIME)
-@ContainerAnnotation(AddActionListener.class)
 public @interface AddActionListeners {
 
     AddActionListener[] value() default {};
